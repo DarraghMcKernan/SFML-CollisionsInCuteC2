93749f897e60677b83d0ee87cf735b4ffda465ee
DarraghMcKernan <93271170+DarraghMcKernan@users.noreply.github.com>
2022-11-27 23:57:20 +0000

    defeat

--- src/Game.cpp
+++ src/Game.cpp
@@ -15,6 +15,8 @@ sf::Texture npc_texture;
 sf::Sprite player_sprite;
 sf::Sprite npc_sprite;
 
+sf::CircleShape CircleCollide;
+
 Game::Game(){}
 
 void Game::initialize()
@@ -38,6 +40,10 @@ void Game::initialize()
 	npc->w = 0;
 	npc->h = 0;
 	npc->r = 0;
+
+	CircleCollide.setPosition(600,50);
+	CircleCollide.setRadius(50);
+	CircleCollide.setFillColor(sf::Color::Yellow);
 
 	player_texture.loadFromFile(".//images//player//Player.png");
 	npc_texture.loadFromFile(".//images//npc//npc.png");
@@ -68,53 +74,6 @@ void Game::update()
 		player->update();
 		npc->update();
 
-		//Setup NPC AABB
-		c2AABB aabb_npc;
-		aabb_npc.min = c2V(npc_sprite.getPosition().x, npc_sprite.getPosition().y);
-		aabb_npc.max = c2V(
-			npc_sprite.getPosition().x +
-			npc_sprite.getGlobalBounds().width, 
-			npc_sprite.getPosition().y +
-			npc_sprite.getGlobalBounds().height);
-			
-		//Setup Player AABB
-		c2AABB aabb_player;
-		aabb_player.min = c2V(player_sprite.getPosition().x,player_sprite.getPosition().y);
-		aabb_player.max = c2V(player_sprite.getGlobalBounds().width, player_sprite.getGlobalBounds().height);
-
-		c2Capsule c2Capsule_player;
-		//c2Capsule_player.a = c2V(300,100);
-		//c2Capsule_player.b = c2V(350,100);
-		c2Capsule_player.a.x = 350;
-		c2Capsule_player.a.y = 100;
-		c2Capsule_player.b.x = 400;
-		c2Capsule_player.b.y = 100;
-
-		c2Capsule_player.r = 25;
-
-		c2Circle c2Circle_player;
-		c2Circle_player.p.x = 50;
-		c2Circle_player.p.y = 200;
-		c2Capsule_player.r = 25;
-
-		if(c2AABBtoCapsule(aabb_player,c2Capsule_player))
-		{
-			cout << "Player capsule collide" << endl;
-			player_sprite.setColor(sf::Color::Green);
-		}
-
-		if(c2AABBtoAABB(aabb_npc, aabb_player)){
-			cout << "NPC and Player are in Collision" << endl;
-			player_sprite.setColor(sf::Color::Red);
-		}
-
-		if(c2CircletoAABB(c2Circle_player, aabb_player))
-		{
-			cout << "Circle player collide" << endl;
-			player_sprite.setColor(sf::Color::Blue);
-		}
-		
-
 		// Move the player
 		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 		{
@@ -132,8 +91,54 @@ void Game::update()
 		{
 			player_sprite.move(sf::Vector2f(4.0f, 0.0f));
 		}
-	}
 
+		//Setup NPC AABB
+		c2AABB aabb_npc;
+		aabb_npc.min = c2V(npc_sprite.getPosition().x, npc_sprite.getPosition().y);
+		aabb_npc.max = c2V(
+			npc_sprite.getPosition().x +
+			npc_sprite.getGlobalBounds().width, 
+			npc_sprite.getPosition().y +
+			npc_sprite.getGlobalBounds().height);
+			
+		//Setup Player AABB
+		c2AABB aabb_player;
+		aabb_player.min = c2V(player_sprite.getPosition().x,player_sprite.getPosition().y);
+		aabb_player.max = c2V(player_sprite.getGlobalBounds().width, player_sprite.getGlobalBounds().height);
+
+		c2Capsule c2Capsule_player;
+		//c2Capsule_player.a = c2V(300,100);
+		//c2Capsule_player.b = c2V(350,100);
+		c2Capsule_player.a.x = 100;
+		c2Capsule_player.a.y = 200;
+		c2Capsule_player.b.x = 100;
+		c2Capsule_player.b.y = 200;
+
+		c2Capsule_player.r = 50;
+
+		c2Circle c2Circle_player;
+		c2Circle_player.p.x = 600;
+		c2Circle_player.p.y = 50;
+		c2Circle_player.r = 50;
+
+		if(c2AABBtoCapsule(aabb_player,c2Capsule_player))
+		{
+			cout << "Player capsule collide" << endl;
+			player_sprite.setColor(sf::Color::Green);
+		}
+
+		if(c2AABBtoAABB(aabb_npc, aabb_player)){
+			cout << "NPC and Player are in Collision" << endl;
+			player_sprite.setColor(sf::Color::Red);
+		}
+
+		if(c2CircletoAABB(c2Circle_player, aabb_player))
+		{
+			cout << "Circle player collide" << endl;
+			player_sprite.setColor(sf::Color::Blue);
+			CircleCollide.setFillColor(sf::Color::White);
+		}
+	}
 }
 
 void Game::draw()
@@ -141,6 +146,7 @@ void Game::draw()
 	window->clear();
 	window->draw(player_sprite);
 	window->draw(npc_sprite);
+	window->draw(CircleCollide);
 	//player->draw();
 	//npc->draw();
 	window->display();
